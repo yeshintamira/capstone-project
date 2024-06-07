9d1a94b7d415786a891a2b8c77559ce39b1af89b
yeshintamira <yeshintamirayolanda12@gmail.com>
2024-06-08 03:13:52 +0700

    update home.js

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -15,25 +15,51 @@ const Home = {
     `).join('');
 
     return `
-      <header class="header">
-      </header>
-      <main class="container" id="maincontent">
-      <div class="intro">
-      <h1 class="intro-title">Selamat datang di Budaya Madura (BUDRA)!</h1>
+    
+  
+    <div class="intro-container">
+    <h1 class="intro-title">Selamat datang di Budaya Madura (BUDRA)</h1>
+    <div class="intro">
       <div class="intro-text">
-        <p>Budaya Madura mencerminkan identitas lokal melalui tradisi dan kesenian seperti Tari Muang Sangkal, Karapan Sapi, Sapi Sono', dan Tanean Lanjhang. Upacara Rokat Tase' dan seni pertunjukan seperti Topeng Dalang serta musik Saronen memperkaya ekspresi budaya, membentuk budaya Madura yang dinamis dan berwarna.</p>
-        <button class="explore-button" tabindex="0"><a href="#/budaya">Baca Lebih Lanjut</a></button>
+        <p>
+          Budaya Madura adalah cerminan kekayaan sejarah dan identitas yang khas. Bahasa Madura yang digunakan secara luas oleh penduduknya, dengan beragam dialek yang mencerminkan keragaman budaya di pulau tersebut. Tradisi dan adat istiadat, seperti upacara pernikahan yang sarat makna simbolis dan prosesi adat lainnya, menjadi inti dari kehidupan sosial masyarakat Madura. Selain itu, tradisi Karapan Sapi yang menjadi ikon budaya Madura, menunjukkan kecintaan dan kebanggaan akan warisan leluhur yang diwariskan dari generasi ke generasi. Budaya Madura tidak hanya menjadi aspek penting dalam kehidupan sehari-hari masyarakatnya, tetapi juga menarik perhatian sebagai daya tarik wisata yang unik di Indonesia.
+        </p>
       </div>
     </div>
-      </main>
+    <div class="madura-image-container">
+      <img src="../images/gambar-budra/suramadu.jpg" alt="Madura" class="madura-image" tabindex="0">
+    </div>
+  </div>
+  
+      
       <article class="article-container">
         <div class="article-header">
-        <h1 tabindex="0">Eksplorasi Budaya Madura</h1>
-        <h2 tabindex="0">Berikut kami rekomendasikan artikel tentang BUDRA</h2>
-          
+        <h1 tabindex="0">Popular Articles of BUDRA </h1>
         </div>
-       
+
+    <div class="rekom-card">
+    <div class="card-item">
+        <img src="../images/gambar-budra/muang-sangkal.jpg" alt="Tari Muang Sangkal" class="article-image" tabindex="0">
+        <h1 tabindex="0">Tari Muang Sangkal</h1>
+        <p tabindex="0">Tari Muang Sangkal berasal dari kata 'Muang' yang berarti 'mengusir' dan 'Sangkal' yang berarti 'kesialan'.</p>
+    </div>
+    <div class="card-item">
+        <img src="../images/gambar-budra/karapansapi.jpg" alt="Karapan Sapi" class="article-image" tabindex="0">
+        <h1 tabindex="0">Karapan Sapi</h1>
+        <p tabindex="0">Karapan Sapi adalah balapan sapi yang sangat populer di Madura. </p>
+    </div>
+    <div class="card-item">
+        <img src="../images/gambar-budra/sapisono.jpeg" alt="Sapi Sono'" class="article-image" tabindex="0">
+        <h1 tabindex="0">Sapi Sono'</h1>
+        <p tabindex="0">Tradisi Sapi Sono' adalah kontes kecantikan sapi.</p>
+    </div>
+    
+</div>
+
       </article>
+
+    <!-- Tombol "Lebih Lanjut" di paling bawah -->
+    <button class="next-button" tabindex="0"><a href="#/budaya"> Lebih Lanjut</a></button>
     </main>
     `;
   },
