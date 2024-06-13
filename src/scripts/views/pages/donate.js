542b4ef159e085b0d0555415db92da8c1424d20e
yeshintamira <yeshintamirayolanda12@gmail.com>
2024-06-13 17:57:23 +0700

    update qr code

--- src/scripts/views/pages/donate.js
+++ src/scripts/views/pages/donate.js
@@ -4,6 +4,7 @@ const Donate = {
       <div class="donate-container">
         <h2 tabindex="0">Donasi</h2>
         <div class="qr-code">
+        <h3 tabindex="0">Scan QR Code di bawah untuk berdonasi</h3> 
           <img src="../images/gambar-budra/qr-code.jpg" alt="Logo BANK BRI" tabindex="0" />
           <p tabindex="0">Nama Penerima: Dwi Wahyu Kuncoro Hadi S.</p>
           <p tabindex="0">Nomor Dana: 0815 6337 3106</p>
